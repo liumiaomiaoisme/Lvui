d5095e92496471cb2c4dd45a66e6783b58ad7089
liujing717 <1510362721@qq.com>
2020-04-28 16:55:29 +0800

    button

---
+++ src/components/button/button.vue
@@ -0,0 +1,47 @@
+<template>
+   <!--:disabled='disabled || loading' 禁用如果不是true还要检测loading loading时也要禁用 -->
+    <button 
+      class='el-button' 
+      :autofocus='autofocus'
+      :disabled='disabled || loading'  
+      @click='handleclick(loading,$event)'
+      :class='[{
+          "is-plain":plain,
+          "is-round":round,
+          "is-circle":circle
+        }]'
+      >
+        <span v-if='$slots.default'><slot></slot></span>
+        <!-- 插槽-->
+    </button>
+</template> 
+
+<script>
+export default {
+    name:'lj-button',
+    props:{
+        type:{
+            type:String,
+            default:'primary'
+        },
+        autofocus:Boolean,
+        disabled:Boolean,
+        loading:Boolean,
+        plain:Boolean,
+        round:Boolean,
+        circle:Boolean,
+    },
+    methods: {
+        // 点击事件传递回父组件，同时传递事件参数对象
+        handleclick(loading,e){
+            this.$emit('click',loading,e)
+            console.log(this.$slots.default)
+            // this.$slots.default  是否有默认插槽
+        }
+    },
+}
+</script>
+
+<style lang='sass' scoped>
+@import "./button.scss"
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -13,10 +13,12 @@ Vue.use(VueRouter)
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/button',
+    name: 'button',
+    component: () => import(/* webpackChunkName: "about" */ '../components/button/demo/index.vue')
   }
 ]
 
